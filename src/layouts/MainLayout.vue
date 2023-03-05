095c45e543b6d839795256d153dc9a94bd76a187
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2023-03-05 10:57:24 -0300

    Initial Commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Engaged
+        </q-toolbar-title>
+
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />  
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup () {
+  }
+})
+</script>
