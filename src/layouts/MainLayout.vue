7251f8260a68b15f3370a22b7837bd03b6046924
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2023-03-08 16:37:46 -0300

    feat: remove header and add back to top button on layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,34 +1,32 @@
 <template>
   <q-layout view="hHh Lpr fFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title>
-          Engaged
-        </q-toolbar-title>
-
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-        />  
-      </q-toolbar>
-    </q-header>
-
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-page-scroller
+      position="bottom-right"
+      :scroll-offset="150"
+      :offset="[18, 18]"
+    >
+      <q-btn fab icon="keyboard_arrow_up" class="top-btn" />
+    </q-page-scroller>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
-  setup () {
-  }
-})
+  setup() {},
+});
 </script>
+
+<style lang="scss">
+.top-btn {
+  background-color: $lightGreen;
+  color: white;
+}
+</style>
