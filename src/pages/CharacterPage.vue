8bf1313b89e48358eb078538cfeb2742bc1ddd47
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2023-03-08 14:25:03 -0300

    feat: Organize Character modules call on his page

---
+++ src/pages/CharacterPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { CharacterMain } from 'src/modules/Character';
+</script>
+
+<template>
+  <q-page padding>
+    <CharacterMain />
+  </q-page>
+</template>
+
+<style lang="scss" scoped>
+</style>
