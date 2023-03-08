625c8003a3831c56ee38905ec3951b7d1ca9fc73
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2023-03-08 14:25:16 -0300

    update: Base Text Component

--- src/components/ui/base/BaseText/BaseText.vue
+++ src/components/ui/base/BaseText/BaseText.vue
@@ -21,7 +21,32 @@ const classes = computed(() => {
   <component
     :is="tag"
     :class="classes"
+    class="text"
   >
     <slot />
   </component>
 </template>
+
+<style lang="scss" scoped>
+.text {
+  margin: 0;
+}
+.text-h1 {
+  font-size: 50px;
+  color: $brown;
+  margin: 40px 0 0;
+
+  @media (min-width: $md) { font-size: 70px; }
+
+}
+
+.text-h3 {
+  font-size: 30px;
+  margin: 0 0 10px;
+}
+.text-h4 {
+  font-size: 20px;
+  margin: 0 0 10px;
+}
+
+</style>
