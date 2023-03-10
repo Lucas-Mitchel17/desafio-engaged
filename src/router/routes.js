7c7083df6555e8e4b8bb8f2c83e3e7858f19e61b
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2023-03-09 23:14:40 -0300

    Update: Routes for dinamic routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,21 +1,36 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'characters-list', component: () => import('src/pages/CharactersListPage.vue') },
-      { path: 'character', component: () => import('src/pages/CharacterPage.vue') }
-    ]
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      {
+        path: "characters-list",
+        component: () => import("src/pages/CharactersListPage.vue"),
+        redirect: { path: "characters-list/1" },
+      },
+      {
+        path: "characters-list/:page",
+        component: () => import("src/pages/CharactersListPage.vue"),
+      },
+      {
+        path: "character",
+        component: () => import("src/pages/CharacterPage.vue"),
+        redirect: { path: "characters-list/1" },
+      },
+      {
+        path: "character/:id",
+        component: () => import("src/pages/CharacterPage.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
